f7b8de5e3bcdde488ea2c67aa0e8687796c94461
burton-barnett <burtonbarnett@gmail.com>
2020-04-20 09:30:16 -0700

    The first commit of my function to cache an inverse of a matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix is a set of functions to cache the inverse of an invertible
+## matrix. cacheSolve checks for a cached inversion and inverts if no inversion
+## is cached.
 
-## Write a short comment describing this function
+## makeCacheMatrix sets im (short for inverse matrix) to NULL.  It establishes
+## the functions to get the original matrix, solve for the inverse matrix, and
+## fetch the inverse matrix if it is stored as im.
+
+## Be sure to use an invertible matrix here.  You can check with det(x) if x is
+## a square matrix (nrow = ncol).  det(x) is != 0 if it is invertible.
+
+## make an object y that is makeCacheMatrix(x).  This is passed to cacheSolve.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        if(det(x) == 0) {
+                message("Only for invertible matrices, fool!")
+        }
+        im <- NULL
+        getmatrix <- function() x
+        setinversematrix <- function(solve) im <<- solve
+        fetchinverse <- function() im
+        list(getmatrix = getmatrix,
+             setinversematrix = setinversematrix,
+             fetchinverse = fetchinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve will invert the matrix x if the inversion of x is not already stored as im.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(y, ...) {
+        im <- y$fetchinverse()
+        if(!is.null(im)) {
+                message("You already inverted this matrix! I'll go get the inversion.")
+                return(im)
+        }
+        data <- y$getmatrix()
+        im <- solve(data, ...)
+        y$setinversematrix(im)
+        im
 }
+
+## if you make an object z that is cacheSolve(y), you can check the
+## inversion with zapsmall(x %*% z).  It should return the identity matrix.
